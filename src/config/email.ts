942505fe315936446c29b395ff5fe13312d2495e
Daniel <projects@gtextsoft.com>
2025-11-24 16:00:53 +0100

    Update email transport configuration in email.ts to enable port 587 and set secure to false for improved email service compatibility.

--- src/config/email.ts
+++ src/config/email.ts
@@ -2,8 +2,8 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
   service: "Gmail",
-  // port: 587,
-  // secure: false,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER, // Your email address
     pass: process.env.EMAIL_PASS, // Your email password
